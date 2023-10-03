51d6a494e8139c50c00770e9078da4ac5d6e503c
Student Ellie <eli.kajbaf@gmail.com>
2023-10-03 09:01:35 -0230

    simple functions

---
+++ simple_functions.py
@@ -0,0 +1,14 @@
+
+def hello_world():
+    """
+    Instructions: Implement a function that returns the string Hello World!
+    """
+    return "Hello World!"
+    raise NotImplementedError()
+
+def add_two(x = 0):
+    """
+    Instructions: Return x+2 for input value x
+    """
+    return x+2
+    raise NotImplementedError()
